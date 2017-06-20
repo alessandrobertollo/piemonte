9229e5a81f01b250dd4ccd990cd30aa76825b885
alessandrobertollo <alessandro.bertollo@hotmail.it>
2017-06-20 20:32:48 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,8 @@
  if ($title == "lago-maggiore"){echo '<a href="index.php"/>Torna alla pagina principale</a>';};
  if ($title == "novara") {echo '<a href="index.php"/>Torna alla pagina principale</a>';};
  	?>
-
+<script src="js/vendor/jquery.min.js"> </sript>
+		<script src="js/vendor/bootstrap.min.js"> </script>
 <hr>
 <footer>
 	&copy2016 irigem
